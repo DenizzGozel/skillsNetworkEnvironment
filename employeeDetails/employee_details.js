e93f48cb3465a8dabaaf076aae1990336d7a144f
Deniz Gozel <dgozel@myseneca.ca>
2025-08-28 14:06:43 -0400

    added a new functionality

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,8 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
+    { id: 4, name: 'Deniz Gozel', age: 27, department: 'IT', salary: 55000, specialization: 'JavaScript' },
     //... More employee records can be added here
   ];
 
@@ -29,6 +30,18 @@ const employees = [
     }
     else{
         document.getElementById("employeesDetails").innerHTML = 'No employee has been found with this id';
+    }
+  }
+
+  function findBySpecializationJavascript() {
+    const jsSpecialistsArray = employees.filter( employee => employee.specialization === "JavaScript" );
+    const jsSpecialists = jsSpecialistsArray.reduce( (total, employee) => total + employee.id + ': ' +  employee.name + '- ' + employee.department + '- ' + employee.specialization + '.' , '').split('.').join('<br>');
+
+    if (jsSpecialists) {
+      document.getElementById("employeesDetails").innerHTML = jsSpecialists;
+    }
+    else{
+      document.getElementById("employeesDetails").innerHTML = 'No employee has been found with this specialization';
     }
   }
 
